501cfa41dd8dc04fb784a3df56a0238c9f790bf0
Sba Tester <sbadoc.test@gmail.com>
2020-12-11 02:42:35 +0000

    hr trid

--- index.js
+++ index.js
@@ -8,44 +8,33 @@ const { base64encode, base64decode } = require('nodejs-base64');
 var util = require('util')
 
 // Read our .env properties (hidden unless you are REPL owner)
-const apikey = process.env.apikey;
-const tdturl = process.env.tdturl;
 const rturl = process.env.rturl;
 const rtlendurl =process.env.rtlendurl;
 const sessurl = process.env.sessurl;
-const clearurl = process.env.clearurl;
 const dclurl = process.env.dclurl;
 const licensekey = process.env.licensekey;
 const doclib = process.env.doclib;
 
 // Bring in our JSON config files
-const tdtbody = require('./json/tdtbody.json');
-const tdtheaders = require('./json/tdtheaders.json');
 const rtheaders = require('./json/rtheaders.json');
 const rtbody = require('./json/rtbody.json');
 const sessbody = require('./json/sessbody.json');
 const dclbody = require('./json/dclbody.json');
-const immbody = require('./json/immbody.json');
-const clearbody = require('./json/clearbody.json');
 
 let ispayloadjson = true;
 var now = new Date().getUTCMilliseconds();
 
 // Update our JSON files from .env properties
-tdtheaders["x-api-key"] = apikey; 
 rtbody.client.licenseKey = licensekey;
 rtbody.documentLibraryVersion = doclib;
 dclbody.jobTicket.DocumentLibraryVersion.DocumentLibraryVersion = doclib;
 dclbody.jobTicket.Prefs.LicenseKeyString = licensekey;
 rtbody.documentLibraryVersion = doclib;
-immbody.jobTicket.DocumentLibraryVersion.DocumentLibraryVersion = doclib;
-immbody.jobTicket.Prefs.LicenseKeyString = licensekey;
-// TODO: Move IMM Forwarding Details to .env 
 
 // Configure and Launch the Express Server
 app.use(bodyParser.json({limit: '15mb'})); 
 app.use(bodyParser.text({ type: 'text/plain' }))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.static('public')); // load files from public directory
 app.listen(process.env.PORT || 3000, function() {
   console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
@@ -69,32 +58,16 @@ app.post("/getdocs", function(req, res) {
   }
   getDocs(receivedData).then(
     function(result) { res.send(result); },
-    function(error) { console.log(error); }
+    function(error) { res.send('Error occured. Have to look at REPL logs, but most likely payload related causing the touchless API flow to fail.'); console.log(error); }
   );
 });
 
 // Asynchronous function to handle e2e workflow
 async function getDocs(receivedData) {
-  console.log('B - getDocs() hit...');
   
-  // 1 - CALL TDT 
-  let base64data;
-  if (ispayloadjson) {
-    receivedData = JSON.stringify(receivedData);
-    base64data = base64encode(receivedData); 
-  } else {
-    console.log('C - getDocs() found non-JSON payload...');
-    base64data = base64encode(receivedData);
-    tdtbody.translatorCode = 'txl';
-    tdtbody.mappingData.data = 'csi_empty.json';
-  }
-  writelog('logs/' + now + '_1_tdtinput', receivedData);
-  tdtbody.partnerData = base64data;
-  let tdtResponse = await axios.post(tdturl, tdtbody, { headers: tdtheaders});
-  let buff = Buffer.from(tdtResponse.data.txl, 'base64');
-  let txl = buff.toString('ascii');
-  console.log("1 - TXL received");
-  writelog('logs/' + now + '_1_tdtoutput', txl);
+  // 1 - PARSE XML PAYLOAD IN
+  let txl = receivedData;
+  writelog('logs/' + now + '_1_receive', receivedData);
 
   // 2 - CALL RUNTIME PAYMENT CALC
   let base64payload = base64encode(txl); 
@@ -138,26 +111,9 @@ async function getDocs(receivedData) {
   writelog('logs/' + now + '_6_dclresponse', util.inspect(dclResponse)); // replace circular links since JSON.stringify had issues
   let encodedPdf = dclResponse.data.Result.RenderedFiles[0].Content;
   console.log("6 - Encoded PDF returned");
-  writelog('logs/' + now + '_6_pdf', encodedPdf);
+  writelog('logs/' + now + '_7_pdf', encodedPdf);
 
-  // 7 - CALL DCL AGAIN TO SEND DOCUMENTS TO IMM
-  /*
-  immbody.jobTicket.DataValuesList[0].content = fulltxl;
-  let immResponse = await axios.post(dclurl, immbody, { headers: rtheaders});
-  let forwardingResults = JSON.stringify(immResponse.data.Result.ForwardingResults);
-  console.log('Forwarding Results = ' + forwardingResults);
-  */
-
-  // 8 - ClearSessions 
-  clearbody.session.id = sessionId;  
-  let clearPymtCalc = await axios.post(clearurl, clearbody, { headers: rtheaders});
-  // console.log('Clear PaymentCalc = ' + JSON.stringify(clearPymtCalc.data));
-  clearbody.session.id = lendsessionId;
-  let clearLending = await axios.post(clearurl, clearbody, { headers: rtheaders});
-  // console.log('Clear Lending = ' + JSON.stringify(clearLending.data));
-  console.log('8 - Cleared our Runtime Sessions...');
-
-  let backUrl = '<a href="https://cunexus--sbatester.repl.co">Go Back</a>';
+  let backUrl = '<a href="https://hrtrid--sbatester.repl.co">Go Back</a>';
 
   return('<html>' + backUrl + '<br><br><object style="width: 100%; height: 100%;" type="application/pdf" data="data:application/pdf;base64,' + encodedPdf + '"' + '></object></html>');
 }
